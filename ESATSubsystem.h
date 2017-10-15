65cc1183240d3bc397f6230a144a7acfb8277776
Sergio García-Cuevas González <sgarcia@eusoc.upm.es>
2017-10-16 00:31:33 +0200

    More detailed comments on the methods of ESATSubsystem.

--- ESATSubsystem.h
+++ ESATSubsystem.h
@@ -28,33 +28,44 @@ class ESATSubsystem
     virtual ~ESATSubsystem() {};
 
     // Start the subsystem.
+    // Called from OnBoardDataHandling.beginSubsystems().
     virtual void begin() = 0;
 
     // Return the application process identifier of this subsystem.
-    // Each subsystem must have a unique 11-bit application process identifier.
+    // Each subsystem must have a unique 11-bit application process
+    // identifier.
+    // OnBoardDataHandling uses this to identify the subsystem.
     virtual word getApplicationProcessIdentifier() = 0;
 
     // Handle a telecommand.
+    // OnBoardDataHandling.dispatchTelecommand() forwards telecommands
+    // to their respective ESATSubsystem modules, identified by the
+    // value returned by getApplicationProcessIdentifier().
     virtual void handleTelecommand(ESATCCSDSPacket& telecommand) = 0;
 
     // Fill a packet with the next telemetry packet available.
     // Return true if the operation was successful;
     // otherwise return false.
+    // Called from OnBoardDataHandling.readSubsystemsTelemetry().
     virtual boolean readTelemetry(ESATCCSDSPacket& packet) = 0;
 
     // Fill a packet with the next telecommand packet available.
     // Return true if the operation was successful;
     // otherwise return false.
+    // Called from OnBoardDataHandling.readTelecommand().
     virtual boolean readTelecommand(ESATCCSDSPacket& packet) = 0;
 
     // Return true if there is new telemetry available;
     // Otherwise return false.
+    // Called from OnBoardDataHandling.readSubsystemsTelemetry().
     virtual boolean telemetryAvailable() = 0;
 
     // Update the subsystem.
+    // Called from OnBoardDataHandling.updateSubsystems().
     virtual void update() = 0;
 
     // Send a telemetry packet to this subsystem.
+    // Called from OnBoardDataHandling.writeTelemetry().
     virtual void writeTelemetry(ESATCCSDSPacket& packet) = 0;
 };
 
