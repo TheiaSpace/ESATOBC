f6d046f48c10db4179650b950231228b1f0f2002
Sergio García-Cuevas González <sgarcia@eusoc.upm.es>
2018-10-01 10:47:42 +0200

    Move ESAT_OBCLinesTelemetry to src/ESAT_OBC-telemetry/

--- src/ESAT_OBC-telemetry/ESAT_OBCLinesTelemetry.cpp
+++ src/ESAT_OBC-telemetry/ESAT_OBCLinesTelemetry.cpp
@@ -18,7 +18,7 @@
  * <http://www.gnu.org/licenses/>.
  */
 
-#include "ESAT_OBCLinesTelemetry.h"
+#include "ESAT_OBC-telemetry/ESAT_OBCLinesTelemetry.h"
 #include "ESAT_OBCClock.h"
 #include "ESAT_OBCSubsystem.h"
 #include "ESAT_TelemetryStorage.h"
