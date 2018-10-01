af53f571413959693219b580d142fe32bc26b63d
Sergio García-Cuevas González <sgarcia@eusoc.upm.es>
2018-10-01 11:24:36 +0200

    ESAT_WifiSubsystem.cpp: include ESAT_OBC-peripherals/ESAT_OBCClock.h

--- src/ESAT_WifiSubsystem.cpp
+++ src/ESAT_WifiSubsystem.cpp
@@ -19,7 +19,7 @@
  */
 
 #include "ESAT_WifiSubsystem.h"
-#include "ESAT_OBCClock.h"
+#include "ESAT_OBC-peripherals/ESAT_OBCClock.h"
 
 void ESAT_WifiSubsystemClass::begin(byte buffer[],
                                     const unsigned long bufferLength)
