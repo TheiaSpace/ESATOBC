d9606a6125c9a04838406a7097e35886c3548569
Sergio García-Cuevas González <sgarcia@eusoc.upm.es>
2020-02-24 13:56:06 +0100

    Update copyright years.

--- src/ESAT_OBC-subsystems/ESAT_ThermalPayloadSubsystem.cpp
+++ src/ESAT_OBC-subsystems/ESAT_ThermalPayloadSubsystem.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2019 Theia Space, Universidad Politécnica de Madrid.
+ * Copyright (C) 2019, 2020 Theia Space, Universidad Politécnica de Madrid.
  *
  * This file is part of Theia Space's ESAT OBC library.
  *
